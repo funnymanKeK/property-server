b4903383a908664a8b59ddb7185198ee1a797cee
DESKTOP-LO7HH07\Admin <janekreitter@gmail.com>
2022-10-28 17:03:20 +0200

    init

---
+++ src/main/java/eu/reitter/common/propertystore/PropertyStoreApplication.java
@@ -0,0 +1,13 @@
+package eu.reitter.common.propertystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyStoreApplication.class, args);
+	}
+
+}
